7eb1cf821c9777def4ae8dc0d28da11342d84d66
zbs285 <97569680+zbs285@users.noreply.github.com>
2022-03-27 18:29:06 -0400

    Update RequestMsg.java

--- Iteration2/src/data_structure/RequestMsg.java
+++ Iteration2/src/data_structure/RequestMsg.java
@@ -5,46 +5,51 @@ package data_structure;
  *
  * @author Boshen Zhang
  */
-
+import java.time.LocalTime;
 
 public class RequestMsg {
-    private int from;  // from which floor
-    private int elevatorId;
-    private int movement; // -1 if moving down, 1 if moving up, 0 if stay still
+	private LocalTime time;
+    private int startFloor;
+    private String direction; // -1 if moving down, 1 if moving up, 0 if stay still
     private int destination;
 
-    public RequestMsg(int from, int elevatorId, int movement, int destination){
-        this.from = from;
-        this.elevatorId = elevatorId;
-        this.movement = movement;
+    public RequestMsg(LocalTime time, int startFloor, String direction, int destination){
+        this.time = time;
+        this.startFloor = startFloor;
+        this.direction = direction;
         this.destination = destination;
     }
 
-    public void setFrom(int from) {
-        this.from = from;
+    public void setFrom(LocalTime time) {
+        this.time = time;
     }
     
-    public void setElevatorId(int elevatorId) {
-		this.elevatorId = elevatorId;
+    public void setstartFloor(int startFloor) {
+		this.startFloor = startFloor;
 	}
 
-    public void setMovement(int movement) {
-        this.movement = movement;
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 
     public void setDestination(int destination) {
         this.destination = destination;
     }
 
-    public int getFrom(){
-    	return from;}
-    
-    public int getElevatorId() {
-		return elevatorId;
+    public LocalTime getTime() {
+		return this.time;
 	}
-    public int getMovement(){return  movement;}
+    
+    public int getstartFloor() {
+		return this.startFloor;
+	}
+    public String getDirection(){return this.direction;}
+    
     public int getDestination() { return destination; }
-
-	
+    
+    @Override
+	public String toString() {
+		return this.time + " " + this.startFloor + " " + this.direction + " " + this.destination;
+	}
 
 }
