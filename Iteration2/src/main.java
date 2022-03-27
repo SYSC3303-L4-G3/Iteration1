86bc47e627b9379e9e85ed265ac2fb8d40db5532
zbs285 <97569680+zbs285@users.noreply.github.com>
2022-03-27 18:28:33 -0400

    Update main.java

--- Iteration2/src/main.java
+++ Iteration2/src/main.java
@@ -1,34 +1,35 @@
-import java.util.ArrayList;
-
-
-public class main {
-    public static void main(String[] args)
-    {
-        Thread schedThread, floorThread, elevThread;
-        Scheduler scheduler;
-
-        scheduler = new Scheduler();
-
-        schedThread = new Thread(new Server(scheduler),"The scheduler");
-        floorThread = new Thread(new Elevator(scheduler),"The elevator");
-        elevThread = new Thread(new Floor(scheduler),"The floor");
-
-        schedThread.start();
-        floorThread.start();
-        elevThread.start();
-    }
-}
-
-class Server implements Runnable{
-    private Scheduler scheduler;
-    private ArrayList<String> ingredientsSupply = new ArrayList<>();
-
-    public Server(Scheduler scheduler)
-    {
-        this.scheduler = scheduler;
-    }
-    public void run() {
-    	
-
-    }
-}
+// //import java.util.ArrayList;
+//
+//
+//public class main {
+//    public static void main(String[] args)
+//    {
+//        Thread schedThread, floorThread, elevThread;
+//        Scheduler scheduler;
+//
+//        scheduler = new Scheduler();
+//
+//        schedThread = new Thread(new Server(scheduler),"The scheduler");
+//        elevThread = new Thread(new Elevator(scheduler),"The elevator");
+//        floorThread = new Thread(new Floor(scheduler),"The floor");
+//
+//        elevThread.start();
+//        schedThread.start();
+//        floorThread.start();
+//
+//    }
+//}
+//
+//class Server implements Runnable{
+//    private Scheduler scheduler;
+//    private ArrayList<String> ingredientsSupply = new ArrayList<>();
+//
+//    public Server(Scheduler scheduler)
+//    {
+//        this.scheduler = scheduler;
+//    }
+//    public void run() {
+//    	
+//
+//    }
+//}
